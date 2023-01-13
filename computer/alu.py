fa7f6681e84ab6abb35a4f542de58a394dd7c227
Diego Dias <diego2007jf@gmail.com>
2023-01-13 15:53:33 -0300

    fixed negative flag && refactored output setting

--- computer/alu.py
+++ computer/alu.py
@@ -7,17 +7,35 @@ class ArithmeticLogicUnit:
         self._B = BitArray(0)
         self._output = BitArray(0)
         self._opcode = BitArray(0)
-        self.carry = BitArray(0)
-        self.negative_bit_flag = Bit(0)
+        self._negative_bit_flag = Bit(0)
 
     @property
     def output(self):
         return self._output
 
+    def _set_output(self, value: BitArray):
+        if len(value) > 8:
+            self._negative_bit_flag = Bit(1)
+            string = BitArray.from_bit_array(value[1:])
+            parsed_string = ''.join(reversed(string))
+            self._output = BitArray(parsed_string)
+            return
+        self._negative_bit_flag = Bit(0)
+        self._output = value
+
+
+    @property
+    def carry(self):
+        return self.output.carry
+
     @property
     def zero_bit_flag(self):
         dif = self.A - self.B
         return Bit(1) if dif == BitArray(0) else Bit(0)
+
+    @property
+    def negative_bit_flag(self):
+        return self._negative_bit_flag
 
     @property
     def A(self):
@@ -29,12 +47,12 @@ class ArithmeticLogicUnit:
 
     @A.setter
     def A(self, value: BitArray):
-        self.negative_bit_flag = Bit(0)
+        self._negative_bit_flag = Bit(0)
         self._A = value
 
     @B.setter
     def B(self, value: BitArray):
-        self.negative_bit_flag = Bit(0)
+        self._negative_bit_flag = Bit(0)
         self._B = value
 
     @property
@@ -52,36 +70,28 @@ class ArithmeticLogicUnit:
 
     def ADD(self):
         self._output = self.A + self.B
-        self.carry = self._output.carry
 
     def SUB(self):
         output = self.A - self.B
-        if len(output) > 8:
-            self.negative_bit_flag = Bit(1)
-            string = BitArray.from_bit_array(output[1:])
-            parsed_string = ''.join(reversed(string))
-            self._output = BitArray(parsed_string)
-
-            return
-        self._output = output
+        self._set_output(output)
 
     def INC(self):
-        self._output = self.A + BitArray(1)
+        self._set_output(self.A + BitArray(1))
 
     def DEC(self):
-        self._output = self.A - BitArray(1)
+        self._set_output(self.A - BitArray(1))
 
     def AND(self):
-        self._output = self.A & self.B
+        self._set_output(self.A & self.B)
 
     def OR(self):
-        self._output = self.A | self.B
+        self._set_output(self.A | self.B)
 
     def NOT(self):
-        self._output = ~self.A
+        self._set_output(~self.A)
 
     def XOR(self):
-        self._output = self.A ^ self.B
+        self._set_output(self.A ^ self.B)
 
 
 if __name__ == '__main__':
