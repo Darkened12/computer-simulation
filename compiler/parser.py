b7d6f3e3cfd628dfeafc74ed12c442bc75760985
Diego Dias <diego2007jf@gmail.com>
2023-01-16 05:33:35 -0300

    implemented subroutines

--- compiler/parser.py
+++ compiler/parser.py
@@ -62,38 +62,74 @@ def _parse_data_section(assembly_code: List[str]) -> List[Dict[str, str]]:
     return parsed_lines
 
 
+def _parse_line(line: str) -> Dict[str, str]:
+    split_line = line.replace(', ', ',').split(' ')
+
+    if len(split_line) != 2:
+        raise CompilerError(f'"{line}" should contain only 2 statements: "operation ref1, ref2"')
+
+    statements = split_line[1].split(',')
+
+    parsed_line = {
+        'operation': split_line[0],
+        'first_statement': statements[0],
+        'second_statement': statements[1] if len(statements) > 1 else None
+    }
+    return parsed_line
+
+
 def _parse_text_section(assembly_code: List[str]) -> List[Dict[str, str]]:
     parsed_lines = []
     for line in assembly_code:
-        split_line = line.replace(', ', ',').split(' ')
-
-        if len(split_line) != 2:
-            raise CompilerError(f'"{line}" should contain only 2 statements: "operation ref1, ref2"')
-
-        statements = split_line[1].split(',')
-
-        parsed_line = {
-            'operation': split_line[0],
-            'first_statement': statements[0],
-            'second_statement': statements[1] if len(statements) > 1 else None
-        }
-
+        parsed_line = _parse_line(line)
         parsed_lines.append(parsed_line)
     return parsed_lines
+
+
+def _parse_subroutines_section(assembly_code: List[str]) -> List[Dict[str, str]]:
+    subroutines: List[Dict[str, str]] = []
+    current_subroutine: Dict[str, str | List] = {}
+    for line in assembly_code:
+        if line.endswith(':'):
+            if current_subroutine != {}:
+                raise CompilerError(f'Missing ret statement on subroutine "{current_subroutine["label"]}"')
+
+            current_subroutine = {'label': f'{line.replace(":", "")}', 'lines': []}
+        elif line == 'ret':
+            subroutines.append(current_subroutine)
+            current_subroutine = {}
+        else:
+            if current_subroutine:
+                current_subroutine['lines'].append(_parse_line(line))
+            else:
+                current_subroutine = {}
+
+    if current_subroutine:
+        raise CompilerError(f'Missing ret statement on subroutine "{current_subroutine["label"]}"')
+
+    return subroutines
 
 
 def _parse_sections(assembly_code: List[str]) -> Dict[str, List[Dict[str, Optional[str]]]]:
     """Returns a dictionary containing the lines separated in sections with the following design:
         dict = {
             'data': [],
+            'subroutines': [],
             'text': [],
         }
     """
     data_section = _extract_section('data', assembly_code)
     text_section = _extract_section('text', assembly_code)
+    subroutines_section = _extract_section('subroutines', assembly_code)
+
+    parsed_data_section = _parse_data_section(data_section)
+    parsed_text_section = _parse_text_section(text_section)
+    parsed_subroutines_section = _parse_subroutines_section(subroutines_section)
+
     result = {
-        'data': _parse_data_section(data_section),
-        'text': _parse_text_section(text_section),
+        'data': parsed_data_section,
+        'subroutines': parsed_subroutines_section,
+        'text': parsed_text_section,
     }
 
     return result
@@ -116,4 +152,4 @@ def get_parsed_code_from_file(path_to_assembly_file: str) -> Dict[str, List[Dict
 if __name__ == '__main__':
     import json
 
-    print(json.dumps(obj=get_parsed_code_from_file('../scripts/count_to_ten.asm'), indent=2))
+    print(json.dumps(obj=get_parsed_code_from_file('../scripts/test.asm'), indent=2))
