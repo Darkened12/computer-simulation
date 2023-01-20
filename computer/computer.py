46fdde938510f8a98afaa7fc36fc33257597ae21
Diego Dias <diego2007jf@gmail.com>
2023-01-20 17:43:40 -0300

    refactoring for the new 8 bit 2 fetch phase cpu

--- computer/computer.py
+++ computer/computer.py
@@ -8,15 +8,15 @@ from .memory import RandomAccessMemory
 class Computer:
     def __init__(self, clock_speed_limiter_in_hertz: int = 0):
         self._alu = ArithmeticLogicUnit()
-        self._ram = RandomAccessMemory(size_in_bytes=16)
-        self._cpu = CentralProcessingUnit(
+        self._ram = RandomAccessMemory(size_in_bytes=256)
+        self.cpu = CentralProcessingUnit(
             alu=self._alu, ram=self._ram, clock_speed_limiter_in_hertz=clock_speed_limiter_in_hertz
         )
         self._total_run_time = 0
 
     @property
     def ram(self):
-        return self._cpu.ram
+        return self.cpu.ram
 
     @property
     def alu(self):
@@ -24,25 +24,25 @@ class Computer:
 
     def run(self):
         start_time = time.perf_counter()
-        while not self._cpu.halt:
-            self._cpu.cycle()
+        while not self.cpu.halt:
+            self.cpu.cycle()
         self._total_run_time = time.perf_counter() - start_time
 
     def status(self):
         print(f'-----------------------\n'
               f'execution took: {self._total_run_time} seconds\n'
-              f'cycles: {self._cpu.cycle_counter}\n'
-              f'average clock speed: {self._cpu.cycle_counter / self._total_run_time} Hz\n'
+              f'cycles: {self.cpu.cycle_counter}\n'
+              # f'average clock speed: {self.cpu.cycle_counter / self._total_run_time} Hz\n'
               f'-----------------------\n'
-              f'ax: {self._cpu.register_A}\n'
-              f'bx: {self._cpu.register_B}\n'
-              f'cx: {self._cpu.register_C}\n'
-              f'dx: {self._cpu.register_D}\n'
+              f'ax: {self.cpu.register_A}\n'
+              f'bx: {self.cpu.register_B}\n'
+              f'cx: {self.cpu.register_C}\n'
+              f'dx: {self.cpu.register_D}\n'
               f'-----------------------\n'
-              f'ac: {self._cpu.accumulator_register}\n'
-              f'sr: {self._cpu.status_register}\n'
-              f'pc: {self._cpu.program_counter_register}\n'
-              f'sp: {self._cpu.stack_pointer}\n'
+              f'ac: {self.cpu.accumulator_register}\n'
+              f'sr: {self.cpu.status_register}\n'
+              f'pc: {self.cpu.program_counter_register}\n'
+              f'sp: {self.cpu.stack_pointer}\n'
               f'-----------------------\n'
               f'ram:\n'
               f'{self.ram}')
